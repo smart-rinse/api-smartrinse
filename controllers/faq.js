673635243c48d285ab723fc2147e056a7d7e5f21
Asep Irfan Setiawan <125580866+asepirfans@users.noreply.github.com>
2023-06-16 14:48:58 +0700

    Update faq.js

--- controllers/faq.js
+++ controllers/faq.js
@@ -1,38 +1,33 @@
 const faq = [
     {
         "id" : "1",
-        "question": "Bagaimana cara kerja aplikasi ini?",
-        "answer": "Aplikasi ini memungkinkan Anda untuk melakukan pemesanan laundry dengan menginisiasi percakapan melalui chat dengan penyedia jasa laundry."
+        "question": "Apa itu aplikasi laundry?",
+        "answer": "Aplikasi laundry adalah sebuah platform teknologi yang memungkinkan pengguna untuk menemukan layanan laundry berkualitas, memesan jasa laundry, dan memberikan umpan balik tentang pengalaman mereka."
     },
     {
         "id" : "2",
-        "question": "Bagaimana cara menemukan layanan laundry untuk diajak berchatting?",
-        "answer": "Aplikasi ini menyediakan daftar penyedia jasa laundry yang mendukung pemesanan melalui chat. Anda dapat menjelajahi opsi yang tersedia, melihat profil mereka, dan memulai percakapan dengan penyedia jasa pilihan Anda."
+        "question": "Bagaimana cara menggunakan aplikasi laundry?",
+        "answer": "Untuk menggunakan aplikasi laundry, Anda perlu mengunduh aplikasi dari playsotore, dan membuat akun pengguna. Setelah itu, Anda dapat mencari laundry terdekat, melihat ulasan dan rating, memilih layanan yang diinginkan, dan melakukan pemesanan melalui aplikasi."
     },
     {
         "id" : "3",
-        "question": "Bagaimana cara membayar layanan laundry?",
-        "answer": "Metode pembayaran dapat bervariasi tergantung pada penyedia jasa. Beberapa mungkin menawarkan opsi pembayaran online melalui aplikasi, sementara yang lain menerima pembayaran tunai atau memberikan pengaturan pembayaran alternatif."
+        "question": "Bagaimana saya dapat memilih laundry yang berkualitas?",
+        "answer": "Aplikasi laundry menyediakan informasi tentang ulasan dan rating dari pelanggan sebelumnya. Anda dapat membaca ulasan tersebut untuk memilih laundry yang memiliki reputasi baik dan memberikan layanan berkualitas."
     },
     {
         "id" : "4",
-        "question": "Jika saya memiliki instruksi khusus untuk pesanan laundry, bagaimana caranya?",
-        "answer": "Anda dapat berkomunikasi langsung dengan penyedia jasa melalui chat untuk memberikan instruksi khusus atau preferensi untuk pesanan laundry Anda. Mereka akan mencatat persyaratan Anda dan memastikan instruksi tersebut diikuti selama proses pencucian."
+        "question": "Bagaimana jika saya tidak puas dengan hasil cuci?",
+        "answer": "Jika Anda tidak puas dengan hasil cuci, Anda dapat memberikan umpan balik melalui aplikasi. Tim pengembang aplikasi akan memperhatikan umpan balik Anda dan dapat memberikan solusi atau tindakan yang diperlukan."
     },
     {
         "id" : "5",
-        "question": "Apakah saya bisa memberikan penilaian dan ulasan untuk layanan laundry setelah menggunakan sistem pemesanan melalui chat?",
-        "answer": "Ya, Anda dapat memberikan umpan balik, memberi penilaian, dan menulis ulasan berdasarkan pengalaman Anda dengan penyedia jasa laundry. Umpan balik ini akan membantu pengguna lain dalam membuat keputusan yang tepat saat memilih layanan laundry."
+        "question": "Apa kelebihan dari aplikasi ini?",
+        "answer": "Aplikasi ini dapat memberikan rekomendasi laundry berdasarkan review dan geolocation, dengan sistem ini Anda dapat mendapatkan layanan laundry yang terbaik"
     },
     {
         "id" : "6",
-        "question": "Apakah informasi pribadi saya dibagikan kepada penyedia jasa selama chat?",
-        "answer": "Informasi pribadi Anda, termasuk percakapan chat, biasanya dijaga kerahasiaannya dan tidak dibagikan kepada pengguna lain atau penyedia jasa. Aplikasi ini harus menjunjung tinggi privasi pengguna dan perlindungan data."
-    },
-    {
-        "id" : "7",
-        "question": "Apa kelebihan dari aplikasi ini?",
-        "answer": "Aplikasi ini dapat memberikan rekomendasi laundry berdasarkan review dan geolocation, dengan sistem ini Anda dapat mendapatkan layanan laundry yang terbaik"
+        "question": "Bagaimana cara melakukan pemesanan melalui aplikasi?",
+        "answer": "Mudah sekali, Anda bisa mencari laundry sesuai dengan pilihan setelah itu Anda dapat memilih layanan laundrynya dan terdapat tombol proses maka pesanan anda akan diterima oleh owner laundrynya"
     },
 ]
 
